1cafc3fcb7e7b017be399798f723cd6868e0bda0
mohammad362 <tashakori.m62@gmail.com>
2023-12-06 12:40:03 +0330

    Add Prometheus Exporter

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/h2non/bimg"
 	lru "github.com/hashicorp/golang-lru/v2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -45,6 +47,16 @@ type Config struct {
 		ExpectContinueTimeout int `mapstructure:"expect_continue_timeout"`
 	} `mapstructure:"http_client"`
 	AllowedHosts map[string]string `mapstructure:"allowed_hosts"`
+	Exporter     struct {
+		BindIP   string `mapstructure:"bind_ip"`
+		Port     int    `mapstructure:"port"`
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+	} `mapstructure:"exporter"`
+	HTTPServer struct {
+		BindIP string `mapstructure:"bind_ip"`
+		Port   int    `mapstructure:"port"`
+	} `mapstructure:"http_server"`
 }
 
 type ImageResult struct {
@@ -53,6 +65,65 @@ type ImageResult struct {
 }
 
 var (
+	requestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "ngax_http_requests_total",
+			Help: "Total number of HTTP requests.",
+		},
+		[]string{"status_code", "method"},
+	)
+
+	responseDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "ngax_http_response_duration_seconds",
+			Help: "Histogram of HTTP response durations.",
+		},
+		[]string{"status_code", "method"},
+	)
+
+	cacheHitsTotal = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "ngax_cache_hits_total",
+			Help: "Total number of cache hits.",
+		},
+	)
+
+	cacheMissesTotal = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "ngax_cache_misses_total",
+			Help: "Total number of cache misses.",
+		},
+	)
+
+	errorsTotal = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "ngax_http_errors_total",
+			Help: "Total number of HTTP errors.",
+		},
+	)
+
+	totalImageSizeBeforeConversion = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "ngax_total_image_size_before_conversion_bytes",
+			Help: "Total size of images before conversion in bytes.",
+		},
+	)
+
+	// Total size of images after conversion
+	totalImageSizeAfterConversion = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "ngax_total_image_size_after_conversion_bytes",
+			Help: "Total size of images after conversion in bytes.",
+		},
+	)
+
+	invalidHostsCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "ngax_invalid_hosts_count",
+			Help: "Count of unauthorized host access attempts.",
+		},
+	)
+
 	config     Config
 	imgCache   *lru.TwoQueueCache[string, []byte]
 	logger     *logrus.Logger
@@ -61,6 +132,15 @@ var (
 )
 
 func init() {
+	prometheus.MustRegister(requestsTotal)
+	prometheus.MustRegister(responseDuration)
+	prometheus.MustRegister(cacheHitsTotal)
+	prometheus.MustRegister(cacheMissesTotal)
+	prometheus.MustRegister(errorsTotal)
+	prometheus.MustRegister(totalImageSizeBeforeConversion)
+	prometheus.MustRegister(totalImageSizeAfterConversion)
+	prometheus.MustRegister(invalidHostsCount)
+
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
@@ -104,11 +184,13 @@ func init() {
 
 func main() {
 	router := mux.NewRouter()
+	metricsRouter := mux.NewRouter()
 	router.HandleFunc("/{image:.*}", handleRequest)
 	router.HandleFunc("/health", healthCheckHandler)
+	metricsRouter.Handle("/metrics", prometheusHandler())
 
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    fmt.Sprintf("%v:%v", config.HTTPServer.BindIP, config.HTTPServer.Port),
 		Handler: router,
 	}
 
@@ -117,8 +199,20 @@ func main() {
 	}()
 
 	go func() {
-		logger.Info("Server started at http://localhost:8080")
+		logger.Info(fmt.Sprintf("Server started at http://%v:%v", config.HTTPServer.BindIP, config.HTTPServer.Port))
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	metricsSrv := &http.Server{
+		Addr:    fmt.Sprintf("%v:%v", config.Exporter.BindIP, config.Exporter.Port),
+		Handler: basicAuthMiddleware(metricsRouter),
+	}
+
+	go func() {
+		logger.Info(fmt.Sprintf("Exporter started at http://%v:%v", config.Exporter.BindIP, config.Exporter.Port))
+		if err := metricsSrv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			logger.Fatalf("ListenAndServe(): %v", err)
 		}
 	}()
@@ -127,15 +221,35 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
 	remoteHost := r.Host
+
+	if r.URL.Path == "/metrics" {
+		// Allow unrestricted access to the /metrics endpoint
+		prometheusHandler().ServeHTTP(w, r)
+		return
+	}
+	if r.URL.Path == "/health" {
+		// Allow unrestricted access to the /health endpoint
+		healthCheckHandler(w, r)
+		return
+	}
 
 	if remoteHost == "" {
 		http.Error(w, "Host header is missing", http.StatusBadRequest)
 		return
 	}
+
+	statusCode := strconv.Itoa(http.StatusOK)
+	duration := time.Since(startTime).Seconds()
+
+	requestsTotal.WithLabelValues(statusCode, r.Method).Inc()
+	responseDuration.WithLabelValues(statusCode, r.Method).Observe(duration)
+
 	// Check if the remote host is allowed and get the corresponding imageURL host
 	allowedHost, exists := config.AllowedHosts[remoteHost]
 	if !exists {
+		invalidHostsCount.Inc()
 		logger.Warn("Unauthorized access attempt from host: ", remoteHost)
 		http.Error(w, "Host not allowed", http.StatusForbidden)
 		return
@@ -151,8 +265,10 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		if cachedImage, found := imgCache.Get(imageURL); found {
 			logger.Info("Cache hit for URL: ", imageURL)
 			serveCachedImage(w, cachedImage)
+			cacheHitsTotal.Inc()
 			return
 		}
+		cacheMissesTotal.Inc()
 	}
 
 	logger.Info("Cache miss for URL: ", imageURL)
@@ -173,6 +289,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	if result.Error != nil {
 		logger.WithFields(logrus.Fields{"error": result.Error.Error(), "url": imageURL}).Error("Error processing image")
 		http.Error(w, fmt.Sprintf("Error processing image: %v", result.Error), http.StatusInternalServerError)
+		errorsTotal.Inc()
 		return
 	}
 
@@ -205,6 +322,8 @@ func processImageAsync(imageURL string, resultChan chan ImageResult) {
 		resultChan <- ImageResult{Error: fmt.Errorf("Error reading image body: %v", err)}
 		return
 	}
+	// Calculate the size of the original image
+	totalImageSizeBeforeConversion.Add(float64(len(body)))
 
 	options := bimg.Options{
 		Quality:  config.WebP.Quality,
@@ -213,6 +332,7 @@ func processImageAsync(imageURL string, resultChan chan ImageResult) {
 	}
 
 	newImage, err := bimg.NewImage(body).Process(options)
+	totalImageSizeAfterConversion.Add(float64(len(newImage)))
 	if err != nil {
 		resultChan <- ImageResult{Error: fmt.Errorf("Error converting image: %v", err)}
 		return
@@ -273,4 +393,24 @@ func isAllowedHost(host string) bool {
 		}
 	}
 	return false
+}
+
+func prometheusHandler() http.Handler {
+	return promhttp.Handler()
+}
+
+func basicAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username := config.Exporter.User
+		password := config.Exporter.Password
+
+		user, pass, ok := r.BasicAuth()
+		if !ok || user != username || pass != password {
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
